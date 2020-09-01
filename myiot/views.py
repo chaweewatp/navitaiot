fd71afc97e237d081039fea6adfaac114c21862b
pornchai chaweewat <chaweewat.p@gmail.com>
2020-09-01 22:06:51 +0700

    edit myiot views

--- myiot/views.py
+++ myiot/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 
+
+@csrf_exempt
 def home(request):
-    print ('Raw Data: "%s"' % request.body)
+    print('Raw Data: "%s"' % request.body)
     return HttpResponse("OK")
