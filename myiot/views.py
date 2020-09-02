c4f251acd8f61e25f984b5e288683fa943be9954
pornchai chaweewat <chaweewat.p@gmail.com>
2020-09-02 15:51:10 +0700

    add request.header

--- myiot/views.py
+++ myiot/views.py
@@ -18,6 +18,7 @@ def testReport(request):
 
 @csrf_exempt
 def testControl(request):
+    print(request.headers())
     print('Raw Data: "%s"' % request.__dict__)
     print('Body Data: "%s"' % request.body)
     return HttpResponse("OK")
