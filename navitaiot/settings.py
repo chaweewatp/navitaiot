5d1356ccd95d5260e4b7fd9d6ccba391bed196d9
chaweewatp <chaweewat.p@gmail.com>
2020-12-20 19:30:54 +0700

    add cors origin

--- navitaiot/settings.py
+++ navitaiot/settings.py
@@ -143,7 +143,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://example.com",
     "https://sub.example.com",
     "http://localhost:8080",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
     "https://navitaiot.herokuapp.com"
 ]
 
