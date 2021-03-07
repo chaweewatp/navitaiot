437710801590ebe875e3a637bda497daa0ecdb81
pornchaic <chaweewat.p@gmail.com>
2021-03-07 20:32:54 +0700

    print function name

--- navitaiot/urls.py
+++ navitaiot/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myiot.urls')),
-    path('', include('myFarm.urls'))
+    path('', include('myFarm.urls')),
+    path('', include('myAPI.urls'))
 
 ]
